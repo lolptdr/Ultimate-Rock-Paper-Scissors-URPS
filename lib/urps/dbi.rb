a977c95ad926369dbd3542c486b86a9a0a85b4bf
Joseph Tingsanchali <charnwoot@gmail.com>
2014-08-06 10:40:37 -0500

    added initial files for module Arena and user

--- lib/urps/dbi.rb
+++ lib/urps/dbi.rb
@@ -21,7 +21,8 @@ module Arena
 			@db.exec(%q[
 				CREATE TABLE IF NOT EXISTS matches(
 					id serial NOT NULL PRIMARY KEY,
-					game integer,
+					user_id integer REFERENCES users(id),
+					game serial NOT NULL,
 					status varchar(30),
 					opponent varchar(30),
 					created_at timestamp NOT NULL DEFAULT current_timestamp
@@ -30,6 +31,7 @@ module Arena
 			@db.exec(%q[
 				CREATE TABLE IF NOT EXISTS games(
 					id serial NOT NULL PRIMARY KEY,
+					matches_id integer REFERENCES matches(id),
 					status varchar(30),
 					opponent varchar(30),
 					p1move text,
@@ -37,7 +39,27 @@ module Arena
 					result varchar(30),
 					created_at timestamp NOT NULL DEFAULT current_timestamp
 				)])
+
+			@db.exec(%q[
+				CREATE TABLE IF NOT EXISTS records(
+					id serial NOT NULL PRIMARY KEY,
+					user_id integer REFERENCES users(id),
+					total_matches integer,
+					match_wins integer,
+					match_losses integer,
+					game_wins integer,
+					game_losses integer,
+					game_ties integer,
+					favorite_move text,
+					highest_winning_move_vs_opponent text,
+					highest_losing_move_vs_opponent text,
+					%rock double precision,
+					%paper double precision,
+					%scissors double precision,
+					created_at timestamp NOT NULL DEFAULT current_timestamp
+				)])
 		end
+
 
 		def self.dbi
 		  @__db_instance ||= DBI.new
